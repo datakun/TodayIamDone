9d482c9cc71e3400657a2ef55e5a225c1f021121
datakun <kimdatagoon@gmail.com>
2020-09-18 05:06:24 +0900

    .gitignore applied

---
+++ week-2/TodayIamDone/app/src/main/java/com/example/todayiamdone/TodoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todayiamdone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+    }
+}
