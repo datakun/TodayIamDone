98be03fbc576d27db97aa2b2e6d900bd7255be9d
datakun <kimdatagoon@gmail.com>
2020-09-23 00:18:10 +0900

    update: 2주차 소스코드 업로드

---
+++ week-2/TodayIamDone/app/src/main/java/com/example/todayiamdone/TodoActivity.kt
@@ -0,0 +1,15 @@
+package com.example.todayiamdone
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_todo.*
+
+class TodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+    }
+}
